4a6b012c2cb4b94c16e4b49c348aa4afb7aa2a16
aariz <aarizmohammed@MacBook-Pro-3.local>
2024-02-07 10:47:20 -0500

    second commit

--- W5A_Todo_App/MainPage.xaml.cs
+++ W5A_Todo_App/MainPage.xaml.cs
@@ -2,24 +2,17 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+	
 
 	public MainPage()
 	{
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+    void EnterTaskButton_Clicked(System.Object sender, System.EventArgs e)
+    {
+    }
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
 }
 
 
